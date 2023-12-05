55a345ed47b3d301f479ed27f7e30b9037b0210d
Álvaro Braz <alvarobraz83@gmail.com>
2023-12-05 12:39:56 -0300

    when

--- src/app/deferrable/deferrable.component.ts
+++ src/app/deferrable/deferrable.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common'
 import { FirstComponentComponent } from '../first-component/first-component.component';
+import { Observable, delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-deferrable',
   standalone: true,
-  imports: [FirstComponentComponent],
+  imports: [FirstComponentComponent, AsyncPipe],
   templateUrl: './deferrable.component.html',
   styleUrl: './deferrable.component.scss'
 })
 export class DeferrableComponent {
+
+  public isTrue: boolean = false
+
+  public carregar(): void {
+    this.isTrue = true
+  }
+
+  public users$: Observable<Array<{ id: number, name: string }>> = of(
+    [
+      { id: 1, name: "Álvaro Braz" },
+      { id: 2, name: "Arthur Máximus da Cruz Braz" },
+      { id: 3, name: "Cristine Schio" }
+    ]
+  ).pipe(delay(5000))
 
 }
